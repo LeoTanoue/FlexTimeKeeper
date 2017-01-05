acbfbc317974ee4d2b0507432cdff87820d29c73
LeoTanoue <leo.tanoue@gmail.com>
2017-01-04 20:41:39 -0600

    Initial Commit

---
+++ FlexTimeKeeper/FlexTimeKeeper/Controllers/UserInfoController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FlexTimeKeeper.Models;
+
+namespace FlexTimeKeeper.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/UserInfo")]
+    public class UserInfoController : ApiController
+    {
+        private FlexTimeKeeperEntities db = new FlexTimeKeeperEntities();
+
+        // GET: api/UserInfo
+        [AllowAnonymous]
+        [Route("allUsers")]
+        public IQueryable<UserInfo> GetUserInfoes()
+        {
+            return db.UserInfoes;
+        }
+
+        // GET: api/UserInfo/5
+        [AllowAnonymous]
+        [Route("allUsers")]
+        public string GetUserInfo(int id)
+        {
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return "not found";
+            }
+
+            return (userInfo.userName);
+        }
+
+        // PUT: api/UserInfo/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUserInfo(int id, UserInfo userInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != userInfo.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(userInfo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/UserInfo
+        [ResponseType(typeof(UserInfo))]
+        public IHttpActionResult PostUserInfo(UserInfo userInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.UserInfoes.Add(userInfo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = userInfo.ID }, userInfo);
+        }
+
+        // DELETE: api/UserInfo/5
+        [ResponseType(typeof(UserInfo))]
+        public IHttpActionResult DeleteUserInfo(int id)
+        {
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            db.UserInfoes.Remove(userInfo);
+            db.SaveChanges();
+
+            return Ok(userInfo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserInfoExists(int id)
+        {
+            return db.UserInfoes.Count(e => e.ID == id) > 0;
+        }
+    }
+}
